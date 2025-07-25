42b0a534bfe989db281824e4e338aa7e0dcc3f06
lin00214 <lin00214@algonquinlive.com>
2025-07-25 18:44:02 -0400

    add the latitude and longitude in console

--- BuggyTasks/Views/LocationPage.xaml.cs
+++ BuggyTasks/Views/LocationPage.xaml.cs
@@ -10,62 +10,19 @@ public partial class LocationPage : ContentPage
     {
         InitializeComponent();
     }
-
+    
     protected override void OnAppearing()
     {
         base.OnAppearing();
-        _ = GetLocationAsync();
+        _ = GetLocation(); 
     }
-
-    private async void OnGetLocationButton(object sender, EventArgs e)
+    
+    async Task GetLocation()
     {
-        await GetLocationAsync();
-    }
-
-    private async Task GetLocationAsync()
-    {
-        try
+        var location = await Geolocation.GetLastKnownLocationAsync(); 
+        if (location != null)
         {
-            StatusLabel.Text = "Getting location...";
-            
-            var status = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
-            if (status != PermissionStatus.Granted)
-            {
-                status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
-            }
-
-            if (status != PermissionStatus.Granted)
-            {
-                StatusLabel.Text = "Location permission denied";
-                return;
-            }
-
-            var location = await Geolocation.GetLastKnownLocationAsync();
-            if (location != null)
-            {
-                StatusLabel.Text = "Location found!";
-                LatitudeLabel.Text = $"Latitude: {location.Latitude:F6}";
-                LongitudeLabel.Text = $"Longitude: {location.Longitude:F6}";
-                Console.WriteLine($"Lat: {location.Latitude}, Long: {location.Longitude}");
-            }
-            else
-            {
-                StatusLabel.Text = "No location available";
-                LatitudeLabel.Text = "Latitude: -";
-                LongitudeLabel.Text = "Longitude: -";
-            }
-        }
-        catch (FeatureNotSupportedException)
-        {
-            StatusLabel.Text = "Location not supported on this device";
-        }
-        catch (PermissionException)
-        {
-            StatusLabel.Text = "Location permission denied";
-        }
-        catch (Exception ex)
-        {
-            StatusLabel.Text = $"Error: {ex.Message}";
+            Console.WriteLine($"Lat: {location.Latitude}, Long: {location.Longitude}");
         }
     }
 
